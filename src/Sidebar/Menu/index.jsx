d80fe28322665ca0e97082ccac8b3610f2b3e785
disha1010 <diana.saginbaeva@gmail.com>
2017-05-27 03:41:16 +0300

    menu detect if page of its category page is opened now

--- src/Sidebar/Menu/index.jsx
+++ src/Sidebar/Menu/index.jsx
@@ -5,12 +5,24 @@ import ArrowIcon from './Icon';
 import { Nav, ButtonTitle, Wrapper } from './styled';
 
 class Menu extends Component {
+  static isActiveMenu(category, locationPath) {
+    const routes = locationPath.split('/');
+    return category && routes[2] && routes[2] === category;
+  }
+
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpened: false,
+      isOpened: Menu.isActiveMenu(props.category, props.location.pathname),
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(prevState => ({
+      isOpened: prevState.isOpened ||
+        Menu.isActiveMenu(nextProps.category, nextProps.location.pathname),
+    }));
   }
 
   toggle() {
@@ -32,10 +44,15 @@ class Menu extends Component {
 
 Menu.propTypes = {
   title: PropTypes.string,
+  category: PropTypes.string,
   children: PropTypes.node.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
 };
 Menu.defaultProps = {
   title: '',
+  category: '',
 };
 
 export default withRouter(Menu);
